66ad985490ebd3e1d04e4ff8a3caed3a3b749362
Jacks Bruno <jacksbruno@MacBook-Air-de-Jacks.local>
2019-04-04 22:36:09 -0400

    Inserindo imagens no App

--- App.js
+++ App.js
@@ -1,6 +1,45 @@
 /* eslint-disable default-case */
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, View, Text, Button, Alert } from 'react-native';
+import { AppRegistry, StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
+
+class Topo extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={require('./imagens/jokenpo.png')} />
+      </View>
+    );
+  }
+}
+
+class Icone extends Component {
+  render() {
+    if (this.props.escolha === 'Pedra') {
+      return (
+        <View style={styles.cxIcones}>
+          <Text style={styles.txtPropsIcones}>{this.props.jogador}</Text>
+          <Image source={require('./imagens/pedra.png')} />
+        </View>
+      );
+    } else if (this.props.escolha === 'Papel') {
+      return (
+        <View style={styles.cxIcones}>
+          <Text style={styles.txtPropsIcones}>{this.props.jogador}</Text>
+          <Image source={require('./imagens/papel.png')} />
+        </View>
+      );
+    } else if (this.props.escolha === 'Tesoura') {
+      return (
+        <View style={styles.cxIcones}>
+          <Text style={styles.txtPropsIcones}>{this.props.jogador}</Text>
+          <Image source={require('./imagens/tesoura.png')} />
+        </View>
+      );
+    } else {
+      return false;
+    }
+  }
+}
 
 export default class App extends Component {
   constructor(props) {
@@ -29,11 +68,12 @@ export default class App extends Component {
 
     if (escolhaComputador === 'Pedra') {
       if (item === 'Pedra') {
-        result = 'Empate';
+        result = 'Empate!!';
       }
       if (item === 'Papel') {
-        result = 'Usuário GANHOU!';
-      } else {
+        result = 'Você GANHOU!';
+      }
+      if (item === 'Tesoura') {
         result = 'Computador GANHOU!';
       }
     }
@@ -43,20 +83,22 @@ export default class App extends Component {
         result = 'Computador GANHOU!';
       }
       if (item === 'Papel') {
-        result = 'Empate';
-      } else {
-        result = 'Usuário GANHOU!';
+        result = 'Empate!!';
+      }
+      if (item === 'Tesoura') {
+        result = 'Você GANHOU!';
       }
     }
 
     if (escolhaComputador === 'Tesoura') {
       if (item === 'Pedra') {
-        result = 'Usuário GANHOU!';
+        result = 'Você GANHOU!';
       }
       if (item === 'Papel') {
         result = 'Computador GANHOU!';
-      } else {
-        result = 'Empate';
+      }
+      if (item === 'Tesoura') {
+        result = 'Empate!!';
       }
     }
 
@@ -66,15 +108,30 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.conteudo}>
-          <Text style={styles.texto}>Escolha do usuário: {this.state.usuario}</Text>
-          <Text style={styles.texto}>Escolha do computador: {this.state.computador}</Text>
-          <Text style={styles.texto}>Resultado: {this.state.resultado}</Text>
+        <Topo />
+        <View style={styles.cxBotoes}>
+          <View style={styles.cxBtnEncolha}>
+            <TouchableOpacity style={styles.btnEscolha} onPress={() => { this.escolha('Pedra') }}>
+              <Text style={{color: '#fff', fontWeight: '600'}}>Pedra</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.cxBtnEncolha}>
+            <TouchableOpacity style={styles.btnEscolha} onPress={() => { this.escolha('Papel') }}>
+              <Text style={{color: '#fff', fontWeight: '600'}}>Papel</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.cxBtnEncolha}>
+            <TouchableOpacity style={styles.btnEscolha} onPress={() => { this.escolha('Tesoura') }}>
+              <Text style={{color: '#fff', fontWeight: '600'}}>Tesoura</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.botoes}>
-          <Button title="Pedra" onPress={() => { this.escolha('Pedra') }} />
-          <Button title="Papel" onPress={() => { this.escolha('Papel') }} />
-          <Button title="Tesoura" onPress={() => { this.escolha('Tesoura') }} />
+
+
+        <View style={styles.conteudoTxt}>
+          <Text style={styles.txtResultado}>{this.state.resultado}</Text>
+          <Icone escolha={this.state.usuario} jogador='Você' />
+          <Icone escolha={this.state.computador} jogador='Computador' />
         </View>
       </View>
     );
@@ -90,14 +147,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
     marginTop: 40
   },
-  conteudo: {
-    margin: 10,
-  },
   texto: {
     fontSize: 16
   },
-  botoes: {
+  cxBotoes: {
     display: 'flex',
+    flexDirection: 'row',
     justifyContent: 'space-between'
+  },
+  cxBtnEncolha: {
+    width: 100,
+    marginTop: 10
+  },
+  btnEscolha: {
+    backgroundColor: '#ffa500',
+    padding: 10,
+    alignItems: 'center',
+  },
+  conteudoTxt: {
+    margin: 10,
+    alignItems: 'center',
+  },
+  txtResultado: {
+    color: 'green',
+    fontSize: 25,
+    height: 60,
+    marginTop: 20
+  },
+  cxIcones: {
+    alignItems: 'center',
+    margin: 20
+  },
+  txtPropsIcones: {
+    textAlign: 'center',
+    fontSize: 21
   }
 });
